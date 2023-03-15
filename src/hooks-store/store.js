0762411fbdb5223530e35ca2afb8459d37c44c19
Sean Kozen <sgkozen@gmail.com>
2023-03-15 11:46:49 +0900

    Optimizing Hook Store

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -1,46 +1,45 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 let globalState = {};
 //Array full of functions that can be called to update all components using the custom hook
 let listeners = [];
 let actions = {};
 
-export const useStore = () => {
-    const setState = useState(globalState)[1];
+export const useStore = (shouldListen = true) => {
+  const setState = useState(globalState)[1];
 
-    //When dispatch is called, global state is updated
-    //Listeners called, which are just set state calls, causing a rerender
-    const dispatch = (actionIdentifier, payload) => {
-        const newState = actions[actionIdentifier](globalState, payload)
-        globalState = {...globalState, ...newState};
+  //When dispatch is called, global state is updated
+  //Listeners called, which are just set state calls, causing a rerender
+  const dispatch = (actionIdentifier, payload) => {
+    const newState = actions[actionIdentifier](globalState, payload);
+    globalState = { ...globalState, ...newState };
 
-        for (const listener of listeners) {
-            listener(globalState);
-        }
-    };
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
 
-    
-    //Register one listener per component
-    useEffect(() => {
-        listeners.push(setState);
-    
-    //Cleanup function to remove listener when components unmounts    
-    return () => {
-        console.log(listeners)
-        listeners = listeners.filter(li => li !== setState)
+  //Register one listener per component
+  useEffect(() => {
+    if (shouldListen) {
+      listeners.push(setState);
     }
 
+    //Cleanup function to remove listener when components unmounts
+    return () => {
+      if (shouldListen) {
+        listeners = listeners.filter((li) => li !== setState);
+      }
+    };
+  }, [setState, shouldListen]);
 
-
-    }, [setState]);
-
-    return [globalState, dispatch];
-}
+  return [globalState, dispatch];
+};
 
 //Initializes store
 export const initStore = (userActions, initialState) => {
-    if (initialState) {
-        globalState = {...globalState, ...initialState};
-    }
-    actions = {...actions, ...userActions};
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
 };
